a6a329c0c92cb0341311b125c90249c4f00c4044
Fausto-Mendez <85911046+Fausto-Mendez@users.noreply.github.com>
2024-05-17 20:43:13 -0600

    Add files via upload

---
+++ AbarrotesCuevas/JAVASCRIPT/logica.js
@@ -0,0 +1,19 @@
+// Lógica JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí puedes agregar la lógica para manejar eventos, como clics en botones, etc.
+    const searchBar = document.querySelector('.search-bar input');
+    const searchIcon = document.querySelector('.search-icon');
+  
+    searchBar.addEventListener('focus', function() {
+      this.placeholder = '';
+    });
+  
+    searchBar.addEventListener('blur', function() {
+      this.placeholder = 'Busca cualquier producto ...';
+    });
+  
+    searchIcon.addEventListener('click', function() {
+      // Aquí puedes agregar la lógica para realizar la búsqueda
+    });
+  });
+  
